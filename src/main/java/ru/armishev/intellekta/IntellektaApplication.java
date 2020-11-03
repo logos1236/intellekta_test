070b4c58ae43a419593d7e095f2461aad4013dfd
logos <armishev@vigroup.ru>
2020-11-03 20:56:42 +0500

    Init

---
+++ src/main/java/ru/armishev/intellekta/IntellektaApplication.java
@@ -0,0 +1,13 @@
+package ru.armishev.intellekta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellektaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellektaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/armishev/intellekta/IntellektaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.armishev.intellekta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellektaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
